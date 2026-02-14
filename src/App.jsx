0dc6032324423c3bbf28ba08701cfd92e31e3950
IusupovAlimzhan712 <alimzhaniusupov@gmail.com>
2026-02-14 19:12:29 +0800

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -13,26 +13,27 @@ export default function Page() {
 
   const getNoButtonText = () => {
     const phrases = [
-      "No",
-      "Are you sure?",
-      "Really sure?",
-      "Think again!",
-      "Last chance!",
-      "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
-      "Plsss? :( You're breaking my heart",
+      "Нет",
+      "Ты уверена любимка?",
+      "Точно уверена?",
+      "Подумай ещё раз!",
+      "Последний шанс!",
+      "Ну точно нет, дэндэ?",
+      "Ты можешь пожалеть об этом!",
+      "Подумай ещё разок!",
+      "Ты абсолютно уверена?",
+      "Это может быть ошибкой!",
+      "Ну имей совесть!",
+      "Не будь такой холодной!",
+      "Передумала?",
+      "Может, всё-таки передумаешь?",
+      "Это твой окончательный ответ?",
+      "Ты разбиваешь мне сердце ;(",
+      "Это твой окончательный ответ?",
+      "Ты разбиваешь мне сердце ;(",
+      "Плиииз? :( Ты разбиваешь мне сердце",
     ];
+  
 
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
